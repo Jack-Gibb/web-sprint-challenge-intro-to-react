220ce0e78b84f46f21d35a3ef665fa732a53fb62
Jack Gibb <ogrewithissues@yahoo.com>
2023-07-06 20:48:36 -0600

    basic mvp finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
+import styled from 'styled-components';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const CenterText = styled.h1`
+text-align: right;
+font-size: 50px;
+`;
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const CharacterList = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await axios.get('https://swapi.dev/api/people/');
+        setCharacters(response.data);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div>
+      <CenterText>The Characters</CenterText>
+      {characters.map((character) => (
+        <Character key={character.name} name={character.name} />
+      ))}
     </div>
   );
-}
+};
+const App = () => {
+  return <CharacterList />;
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const CharacterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 24px;
+  text-align: right;
+`;
+
+const CharacterName = styled.h2`
+  margin-bottom: 16px;
+`;
+
+const Character = ({ name }) => {
+  return (
+    <CharacterContainer>
+      <CharacterName>{name}</CharacterName>
+    </CharacterContainer>
+  );
+};
+
+export default Character;
